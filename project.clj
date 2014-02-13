bd65d95542d016a46e5ab564d8de5c512a13b5fc
Felix Flores <felixflores@gmail.com>
2014-02-13 00:06:06 -0500

    Update Datomic

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-pro "0.9.4497"]
+                 [com.datomic/datomic-pro "0.9.4532"]
                  [dorothy "0.0.4"]])
