8ee69de41cdb95e0f22d5004d5b0fcdc49a96414
Felix Flores <felixflores@gmail.com>
2014-03-02 15:59:31 -0500

    Remove dev dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,11 @@
-(defproject datomic-schema-grapher "0.1.0-SNAPSHOT"
+(defproject datomic-schema-grapher "0.0.1"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/felixflores/datomic_schema_grapher"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.datomic/datomic-pro "0.9.4572"]
-                 [dorothy "0.0.5"]
-                 [org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.namespace "0.2.4"]
-                 [org.clojure/tools.trace "0.7.6"]]
+  :dependencies [[dorothy "0.0.5"]
+                 [com.datomic/datomic-free "0.9.4572"]]
+  :dev-dependencies [[org.clojure/clojure "1.5.1"]]
   :jvm-opts ["-Xmx1g"]
-  :plugins [[quickie "0.2.5"]]
   :eval-in-leiningen true)
 
