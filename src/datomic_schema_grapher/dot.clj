98bf485bf79eba3264924068b7d7b099e5579a26
Felix Flores <felixflores@gmail.com>
2014-03-03 23:32:40 -0500

    Use html like syntax for better styling of entities

--- src/datomic_schema_grapher/dot.clj
+++ src/datomic_schema_grapher/dot.clj
@@ -1,6 +1,10 @@
 (ns datomic-schema-grapher.dot
   (:require [clojure.string :refer (split join)]
+            [hiccup.core :refer (html)]
             [dorothy.core :refer (subgraph node-attrs digraph dot show! save! graph-attrs)]))
+
+(def light-grey "#808080")
+(def edge-colors ["#C0493F" "#C0573F" "#C06C3F" "#C0823F" "#C0913F"])
 
 (defn group-as-entities
   [schema]
@@ -10,34 +14,40 @@
   [attr]
   (= (:db/valueType attr) :db.type/ref))
 
-(defn attribute-label
+(defn is-unique?
+  [attr]
+  (= (:db/unique attr) :db.unique/identity))
+
+(defn attr-row-label
   [attribute]
-  (let [attr-name (name (:db/ident attribute))
-        attr-type (name (:db/valueType attribute))
-        attr-port (when (is-a-ref? attribute) (str "<" attr-name ">"))]
-    (str attr-port attr-name " : " attr-type)))
+  (let [label (str (name (:db/ident attribute)) " : " (name (:db/valueType attribute)))]
+    (if (is-unique? attribute)
+      (html [:font {:color "red"} label])
+      label)))
 
 (defn node-label
   [attributes]
-  (let [entity-name (namespace (:db/ident (first attributes)))
-        attr-labels (map attribute-label attributes)]
-    {:label (str "{\n" entity-name "\n|" (apply str (interpose "|" attr-labels)) "}")
-     :shape "Mrecord"}))
+  (let [entity-name (namespace (:db/ident (first attributes)))]
+    (html [:table {:port entity-name :border 0 :color light-grey :cellborder 1 :cellspacing 0}
+           [:tr [:td {:bgcolor "gray"} entity-name]]
+           (for [attribute attributes
+                 :let [attr-name (name (:db/ident attribute))]]
+             [:tr [:td (when (is-a-ref? attribute) {:port attr-name}) (attr-row-label attribute)]])])))
 
 (defn dot-nodes
   [schema]
   (for [[entity-name attributes] (group-as-entities schema)]
-    [entity-name (node-label attributes)]))
+    [entity-name {:label (node-label attributes)}]))
 
 (defn dot-relationship
   [[root dest-label cardinality]]
   (let [root-label (str (namespace root) ":" (name root))
         dest-ref-label (str dest-label "_ref")
-        cardinality-label {:arrowhead (if (= cardinality "one") "tee" "crown")}]
+        edge-attrs {:arrowhead (if (= cardinality "one") "tee" "crow")}]
     (if (= (namespace root) dest-label)
-      [[dest-ref-label {:label dest-label :style "dotted,rounded"}]
-       [root-label dest-ref-label cardinality-label]]
-      [[root-label dest-label cardinality-label]])))
+      [[dest-ref-label {:label dest-label :shape "rectangle" :style "dotted,rounded" :color light-grey}]
+       [root-label dest-ref-label edge-attrs]]
+      [[root-label (str dest-label ":" dest-label) edge-attrs]])))
 
 (defn dot-relationships
   [relationships]
@@ -45,7 +55,7 @@
 
 (defn to-dot
   [schema relationships]
-  (dot (digraph (concat [(node-attrs {:shape "record"})]
+  (dot (digraph (concat [(node-attrs {:shape "plaintext"})]
                         (dot-nodes schema)
                         (when (not-empty relationships)
                           (dot-relationships relationships))))))
--- test/datomic_schema_grapher/dot/test.clj
+++ test/datomic_schema_grapher/dot/test.clj
@@ -26,36 +26,43 @@
                :db/cardinality :db.cardinality.one
                :db/valueType :db.type/ref})
 
-(def attr-cardinality-one {:db/ident :entity1/attr1
-                           :db/cardinality :db.cardinality.one
-                           :db/valueType :db.type/string})
+(def attr-string {:db/ident :entity1/attr1
+                  :db/cardinality :db.cardinality.one
+                  :db/valueType :db.type/string})
 
-(def attr-cardinality-many {:db/ident :entity1/attr2
-                            :db/cardinality :db.cardinality.many
-                            :db/valueType :db.type/string})
+(def attr-unique {:db/ident :entity1/attr2
+                  :db/cardinality :db.cardinality.one
+                  :db/unique :db.unique/identity
+                  :db/valueType :db.type/string})
 
 (deftest test-is-a-ref?
   (testing "is-a-ref?"
     (are [x y] (= (is-a-ref? x) y)
          attr-ref true
-         attr-cardinality-one false)))
+         attr-string false)))
 
-(deftest test-attribute-label
-  (testing "node-attribute-label"
-    (are [x y] (= (attribute-label x) y)
-         attr-ref "<reference>reference : ref"
-         attr-cardinality-one "attr1 : string"
-         attr-cardinality-many "attr2 : string")))
+(deftest test-is-unique?
+  (testing "is-unique?"
+    (are [x y] (= (is-unique? x) y)
+         attr-unique true
+         attr-string false)))
+
+(deftest test-attr-row-label
+  (testing "Attribute rows are generated properly"
+    (are [x y] (= (attr-row-label x) y)
+         attr-unique "<font color=\"red\">attr2 : string</font>"
+         attr-string "attr1 : string")))
 
 (deftest test-node-label
   (testing "node-label"
-    (is (= (node-label [attr-ref attr-cardinality-one attr-cardinality-many])
-           {:label "{\nentity1\n|<reference>reference : ref|attr1 : string|attr2 : string}" :shape "Mrecord"}))))
+    (are [x y] (= (node-label [x]) y)
+         attr-ref "<table border=\"0\" cellborder=\"1\" cellspacing=\"0\" color=\"#808080\" port=\"entity1\"><tr><td bgcolor=\"gray\">entity1</td></tr><tr><td port=\"reference\">reference : ref</td></tr></table>"
+         attr-string "<table border=\"0\" cellborder=\"1\" cellspacing=\"0\" color=\"#808080\" port=\"entity1\"><tr><td bgcolor=\"gray\">entity1</td></tr><tr><td>attr1 : string</td></tr></table>")))
 
 (deftest test-dot-relationship
   (testing "Relationships are properly mapped"
     (are [x y] (= (dot-relationship x) y)
-         '(:entity1/multi "entity2" "many") [["entity1:multi" "entity2" {:arrowhead "crown"}]]
-         '(:entity1/self "entity1" "one") [["entity1_ref" {:label "entity1" :style "dotted,rounded"}]
+         '(:entity1/multi "entity2" "many") [["entity1:multi" "entity2:entity2" {:arrowhead "crow"}]]
+         '(:entity1/self "entity1" "one") [["entity1_ref" {:label "entity1", :shape "rectangle", :style "dotted,rounded", :color "#808080"}]
                                            ["entity1:self" "entity1_ref" {:arrowhead "tee"}]])))
 
